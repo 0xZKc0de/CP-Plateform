c8ed81eded1386969f8c205cf1ee94de5d07b244
Mohamed EL HADDAD <elhaddadmohamed963@gmail.com>
2025-11-23 22:33:33 +0100

    Complete submissiom entity

--- cp-springboot/src/main/java/com/g8s6/cp_springboot/model/Submission.java
+++ cp-springboot/src/main/java/com/g8s6/cp_springboot/model/Submission.java
@@ -1,5 +1,25 @@
 package com.g8s6.cp_springboot.model;
 
+import jakarta.annotation.Generated;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+
+@Entity @Data
+@NoArgsConstructor @AllArgsConstructor
 public class Submission {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
     
+    private String code_source;
+    private Language language;
+    private boolean isPassed;
+    private double execution_time;
+    private double submission_time;
+
 }
