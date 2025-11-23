81b6342f04567cb56edfc2691a0b189d377cedef
Mohamed EL HADDAD <elhaddadmohamed963@gmail.com>
2025-11-23 22:38:08 +0100

    Complete problem entity

---
+++ cp-springboot/src/main/java/com/g8s6/cp_springboot/model/Problem.java
@@ -0,0 +1,26 @@
+package com.g8s6.cp_springboot.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+
+@NoArgsConstructor @AllArgsConstructor
+@Data @Entity
+public class Problem {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+    private String description;
+    private double time_limit;
+    private double memory_limit;
+    private String resumeUrl;
+
+    private List<TestCase> testCases;
+    private List<Submission> submissions;
+}
