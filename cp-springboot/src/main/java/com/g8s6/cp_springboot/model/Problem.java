23dbb6bc5dca180003ad2a87b062a867d1bbc7bf
Mohamed EL HADDAD <elhaddadmohamed963@gmail.com>
2025-11-23 23:10:48 +0100

    Complete TestCase entity

--- cp-springboot/src/main/java/com/g8s6/cp_springboot/model/Problem.java
+++ cp-springboot/src/main/java/com/g8s6/cp_springboot/model/Problem.java
@@ -1,5 +1,7 @@
 package com.g8s6.cp_springboot.model;
 
+import java.util.List;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
---
+++ cp-springboot/src/main/java/com/g8s6/cp_springboot/model/TestCase.java
@@ -0,0 +1,23 @@
+package com.g8s6.cp_springboot.model;
+
+import jakarta.annotation.Generated;
+import jakarta.annotation.sql.DataSourceDefinition;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor @AllArgsConstructor
+@Data @Entity
+public class TestCase {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY) 
+    private Long id;
+
+    private String input_data;
+    private String excepted_output;
+}
