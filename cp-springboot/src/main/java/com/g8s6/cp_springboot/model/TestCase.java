6abe6f26cc0a25697e9f3952a58aaa5265a41ace
Mohamed EL HADDAD <elhaddadmohamed963@gmail.com>
2025-11-23 23:11:21 +0100

    Update TestCase entity

--- cp-springboot/src/main/java/com/g8s6/cp_springboot/model/TestCase.java
+++ cp-springboot/src/main/java/com/g8s6/cp_springboot/model/TestCase.java
@@ -1,7 +1,6 @@
 package com.g8s6.cp_springboot.model;
 
 import jakarta.annotation.Generated;
-import jakarta.annotation.sql.DataSourceDefinition;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
