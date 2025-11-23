9e03bade5826918ad0815c860ca35a5038a5b52e
Mohamed EL HADDAD <elhaddadmohamed963@gmail.com>
2025-11-23 21:21:42 +0100

    Initial commit

---
+++ cp-springboot/src/main/java/com/g8s6/cp_springboot/CpSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.g8s6.cp_springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CpSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CpSpringbootApplication.class, args);
+	}
+
+}
---
+++ cp-springboot/src/test/java/com/g8s6/cp_springboot/CpSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.g8s6.cp_springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CpSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
