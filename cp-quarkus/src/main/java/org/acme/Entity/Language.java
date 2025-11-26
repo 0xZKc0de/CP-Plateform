85c690ec42d03d16dad00513cfda5cd08e525e7e
Mohamed EL HADDAD <elhaddadmohamed963@gmail.com>
2025-11-26 12:56:41 +0100

    Start with Language Entity

--- cp-quarkus/src/main/java/org/acme/Entity/Language.java
+++ cp-quarkus/src/main/java/org/acme/Entity/Language.java
@@ -1,5 +1,31 @@
 package org.acme.Entity;
 
-public class Language {
-    
+public enum Language {
+    JAVA("Java", "OpenJDK 17"),
+    PYTHON("Python", "Python 3.10"),
+    CPP("C++", "GNU GCC 17"),
+    JAVASCRIPT("JavaScript", "Node.js 18"),
+    C("C", "GNU GCC 17"),
+    RUBY("Ruby", "Ruby 3.1"),
+    GO("Go", "Go 1.20"),
+    KOTLIN("Kotlin", "Kotlin 1.7"),
+    SWIFT("Swift", "Swift 5.7"),
+    PASCAL("Pascal", "Free Pascal 3.2"),
+    PHP("PHP", "PHP 8.1");
+
+    private final String displayName;
+    private final String version;
+
+    Language(String displayName, String version) {
+        this.displayName = displayName;
+        this.version = version;
+    }
+
+    public String getDisplayName() {
+        return displayName;
+    }
+
+    public String getVersion() {
+        return version;
+    }
 }
