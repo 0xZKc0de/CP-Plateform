53f65bdfaf7178bdd0fae635e396a618206483e6
Mohamed EL HADDAD <elhaddadmohamed963@gmail.com>
2025-11-26 12:56:07 +0100

    Start with Submission Entity

--- cp-quarkus/src/main/java/org/acme/Entity/Submission.java
+++ cp-quarkus/src/main/java/org/acme/Entity/Submission.java
@@ -1,5 +1,24 @@
 package org.acme.Entity;
 
+import jakarta.annotation.Generated;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity 
+@NoArgsConstructor 
 public class Submission {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+	public Long id;
     
+    public String code_source;
+    public Language language;
+    public boolean isPassed;
+    public double execution_time;
+    public double submission_time;
+
 }
