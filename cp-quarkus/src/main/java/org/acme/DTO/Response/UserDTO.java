bc6c113f75b1bea9e01c3b8c6830453b175bad0c
Mohamed EL HADDAD <elhaddadmohamed963@gmail.com>
2025-11-27 22:56:59 +0100

    make first DTO for User

---
+++ cp-quarkus/src/main/java/org/acme/DTO/Response/UserDTO.java
@@ -0,0 +1,8 @@
+package org.acme.DTO.Response;
+
+public class UserDTO {
+    public Long id;
+    public String username;
+    public String email;
+    public int rating;
+}
--- cp-quarkus/src/main/java/org/acme/GreetingResource.java
+++ cp-quarkus/src/main/java/org/acme/GreetingResource.java
@@ -4,8 +4,7 @@ import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
-import org.eclipse.microprofile.config.inject.ConfigProperty; // تأكد من هذا الاستيراد
-
+import org.eclipse.microprofile.config.inject.ConfigProperty;
 @Path("/hello")
 public class GreetingResource {
 
