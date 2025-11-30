bbcdd4c6f4300ed5e0ef9401f054e1a9e001d94b
Mohamed EL HADDAD <elhaddadmohamed963@gmail.com>
2025-11-30 12:14:34 +0100

    Add Problem Repo and Service

--- cp-quarkus/src/main/java/org/acme/Repository/ProblemRepository.java
+++ cp-quarkus/src/main/java/org/acme/Repository/ProblemRepository.java
@@ -1,4 +1,14 @@
 package org.acme.Repository;
 
-public class ProblemRepository {
+import io.quarkus.hibernate.orm.panache.PanacheRepository;
+import jakarta.enterprise.context.ApplicationScoped;
+import org.acme.Model.Problem;
+
+import java.util.List;
+
+@ApplicationScoped
+public class ProblemRepository implements PanacheRepository<Problem> {
+
+
+
 }
