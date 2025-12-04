10108e4aba88defd1c21fd667b7d549d46fa1f1d
Mohamed EL HADDAD <elhaddadmohamed963@gmail.com>
2025-12-04 22:52:06 +0100

    Update the UserService

--- cp-quarkus/src/main/java/org/acme/Service/UserService.java
+++ cp-quarkus/src/main/java/org/acme/Service/UserService.java
@@ -1,9 +1,12 @@
 package org.acme.Service;
 
+import org.acme.Model.User;
 import org.acme.Repository.UserRepository;
-import org.acme.DTO.Response.UserDTO; 
+import org.acme.DTO.Response.UserDTO;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
+import jakarta.transaction.Transactional; 
+import jakarta.ws.rs.NotFoundException;
 
 import java.util.List;
 import java.util.Optional;
@@ -15,11 +18,43 @@ public class UserService {
     UserRepository userRepository;
 
     public List<UserDTO> getUsers() {
-
         return userRepository.findAllProjected();
     }
 
     public Optional<UserDTO> findById(Long id){
         return userRepository.findUserDtoById(id);
+    }
+
+    @Transactional
+    public UserDTO createUser(User userRequest) {
+        userRepository.persist(userRequest);
+
+        return mapToDTO(userRequest);
+    }
+
+    @Transactional
+    public UserDTO updateUser(Long id, User userUpdates) {
+        User user = userRepository.findByIdOptional(id)
+                .orElseThrow(() -> new NotFoundException("User not found"));
+
+        user.username = userUpdates.username;
+        user.email = userUpdates.email;
+        user.password = userUpdates.password; // يفضل تشفير كلمة السر هنا
+        user.rating = userUpdates.rating;
+
+
+        return mapToDTO(user);
+    }
+
+    @Transactional
+    public void deleteUser(Long id) {
+        boolean deleted = userRepository.deleteById(id);
+        if (!deleted) {
+            throw new NotFoundException("User not found");
+        }
+    }
+
+    private UserDTO mapToDTO(User user) {
+        return new UserDTO(user.id, user.username, user.email, user.rating);
     }
 }
